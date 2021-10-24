45a1b39b1af2ef219ad19121b11210fc774e6a6b
Alan Contreras <alanrcontrerasm@gmail.com>
2021-10-24 15:44:23 -0700

    Add basic router switch system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // we can set as attribute <BrowserRouter basename="/tutorial"> and it will add it to the beginning of our url 
+    //we can add <BrowserRouter forceRefresh>, so it forces a refresh and rerendering.
+    //<BrowserRouter getUserConfirmation={(message, callback)=>{callback(window.confirm())}}>
+
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+        <Route path='/' component={Home} exact/>
+        <Route path='/about' component={About} />
+        <Route path='/profile' component={Profile} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
