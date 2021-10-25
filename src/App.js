78927fc6dfd924c15eb56df0f32159b218ec3b4c
Alan Contreras <alanrcontrerasm@gmail.com>
2021-10-24 20:07:25 -0700

    Add conditionals and params example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom"
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import NotFound from "./pages/NotFound";
+import Post from "./pages/Post";
+import { useState } from "react";
 
 function App() {
+
+  const [login, setLogin] = useState(false);
+
   return (
     // we can set as attribute <BrowserRouter basename="/tutorial"> and it will add it to the beginning of our url 
     //we can add <BrowserRouter forceRefresh>, so it forces a refresh and rerendering.
@@ -14,10 +20,28 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
+        <button className="btn btn-outline-success my-2" onClick={()=>{setLogin(!login)}}>{login?"logout":"login"}</button>
+
         <Switch>
         <Route path='/' component={Home} exact/>
         <Route path='/about' component={About} />
-        <Route path='/profile' component={Profile} />
+        {/* for login and nested stuff */}
+        
+        
+        {/* <Route path='/profile' component={Profile} /> */}
+        <Route path='/profile'>
+          {/* 
+            We ca either do direct like this so it checks login status and redirects if false
+            {login?<Profile/>:<Redirect to="/"/>}
+            or useHistory, check inside the profile page, I myself would rather use the first option for simple validation.
+          */}
+          <Profile login={login}/>
+        </Route> 
+
+
+        {/* for params */}
+        <Route path='/post/:id' component={Post} />
+        <Route component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,29 @@
-function Profile() {
+import { useHistory } from "react-router";
+import React, { useEffect } from 'react';
+
+
+function Profile({login}) {
+    const history = useHistory();
+
+    useEffect(() => {
+        if(!login){
+            history.push("/");
+        }
+    }, [login, history]);
+
+
     return ( 
-        <h1>Profile Page</h1>
+        <>
+            <h1>Profile Page</h1>
+            {/* <ul>
+                <li>
+                    <Link>View</Link>
+                </li>
+                <li>
+                    <Link>Edit</Link>
+                </li>
+            </ul> */}
+        </>
      );
 }
 
